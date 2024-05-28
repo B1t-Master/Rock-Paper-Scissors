2a805e3c5726373a6d93f480ffe383e6e2fadc0b
B1t-Master <isaac.njuguna@strathmore.edu~.noreply.github.com>
2024-05-28 10:48:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rock-Paper-Scissors
+TOP - first javascript project
+This project will be an entry into problem solving 
+I will be demonstrating my aquired skills in Javascript thus far being:
+* Functions 
+* Conditional statements 
+* Operators 
+Down the line I plan to eventually add a GUI for the game, In the meanwhile it can be acessed via the console
---
+++ script.js
@@ -0,0 +1,16 @@
+/* Pseudocode
+   Ask the user for the number of rounds they want to play
+   Insert the number of rounds the user wants to play and loop first get the logic for 1 round do this later
+   Randomly generate the computers choice(using numbers(1-rock, 2-paper , 3-scissors))
+   Input the users choice (Rock, Paper or Scissors)
+   if computer chooses 1 and user chooses rock display tie  
+   if computer chooses 1 and user chooses paper display player wins ,increment palyer score by 1 
+   if computer chooses 1 and user chooses scissors display computer wins , increment computer score by 1
+   if computer chooses 2 and user chooses paper display tie
+   if computer chooses 2 and user chooses rock display computer wins , computer score +1
+   if computer chooses 2 and user chooses scissors display player wins , player score +1
+   if computer chooses 3 and user chooses scissors display tie
+   if computer chooses 3 and user chooses rock display player wins , player score +1
+   if computer chooses 3 and user chooses paper display computer wins , computer score +1
+   Compare the scores at the end and display win lose or tie
+   */
